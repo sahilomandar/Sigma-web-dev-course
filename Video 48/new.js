9da7f455ebf3434889e8a91d45aeaf661b509bf0
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-07 21:36:19 +0530

    Introduction to JavaScript

---
+++ Video 48/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("Code is Running...")
+console.log("Code is Running Fast...")
+console.log("Code is Running at Fastest Speed...")
---
+++ Video 48/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("Code is Running...")
+console.log("Code is Running Fast...")
+console.log("Code is Running at Fastest Speed...")
+
+var a = prompt("Enter your Number")
+var istrue = confirm("Are you sure you want to leave this page and Blast your Computer ")
+
+if(istrue){
+    console.log("Computer is Blasting")
+}
+
+else{
+    console.log("Computer is not Blasting")
+}
+
+console.log("Your Number is " + a)
+
+// document.title = "sahil"
+
+document.body.style.backgroundColor = "yellow"
