56aabbd0c84ec0c8057b1e2aa9963705f998b3b7
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-09 21:35:33 +0530

    JavaScript Variables, Data Types & Objects

--- Video 49/script.js
+++ Video 49/script.js
@@ -1,8 +1,45 @@
 console.log("Hey I am Learnig Javascript")
 
-var a = 5;
-var b = 10;
-var c = "Sahil";
+let a = 5;
+// a = a + 1;
+let b = 10;
+let c = "Sahil";
+let _b = "Aniket";
+// var 44a = "hey";
+// Invalid never start with Number
 
-console.log(a + b + 11)
-console.log(typeof a, typeof b, typeof c)
+// console.log(a + b + 11);
+// console.log(typeof a, typeof b, typeof c );
+
+{
+    // let a = 66;
+    console.log(a)
+}
+
+console.log(a)
+
+// const a = 10;
+// a1 = a1 + 1;
+// Not Allowed because a1 is constant
+
+let x = "Sahil Bro"
+let y = "22"
+let z = "0.2"
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "Sahil",
+    "Job Code" : 2525,
+    "is_goodlookinng": true
+}
+
+console.log(o);
+o.salary = "250crores";
+console.log(o);
+o.salary = "550crores";
+console.log(o);
