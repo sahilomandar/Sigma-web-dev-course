1d90ff7d2bf2caa0febcf8d48f3f97639109c30b
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-10 20:20:42 +0530

    Use typeof operator to find the datatype of the string in last question

---
+++ Video 51/script.js
@@ -0,0 +1,11 @@
+const data = {
+     "name" : "Sahil",
+     "phone number" : 2255846667,
+     "marks" : 525
+     
+}
+
+const nametype = typeof data.name;
+
+console.log(data);
+console.log(nametype);
