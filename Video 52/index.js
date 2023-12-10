908932f47828701b0f5c07c48af2ea79490198be
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-10 21:03:07 +0530

    Conditional Tutorial

---
+++ Video 52/index.js
@@ -0,0 +1,12 @@
+console.log("Hello I am Conditional Tutorial")
+
+let age = 15;
+let grace = 3;
+
+if((age+grace)>=18){
+    console.log("Yes you can drive");
+}
+
+else{
+    console.log("No you cannot drive");
+}
