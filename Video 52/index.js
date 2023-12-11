e099f7fcd862496c287bde2dae39f83d9c6cb2d3
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-11 21:37:05 +0530

    JavaScript Conditionals: if, else if, else ladder

--- Video 52/index.js
+++ Video 52/index.js
@@ -1,12 +1,45 @@
 console.log("Hello I am Conditional Tutorial")
 
-let age = 15;
-let grace = 3;
+let age = 1;
+// let grace = 3;
 
-if((age+grace)>=18){
+// age += grace
+
+// console.log(age);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age ** grace);
+// console.log(age / grace);
+// console.log(age % grace);
+
+
+if(age==18){
     console.log("Yes you can drive");
+}
+
+else if(age == 0){
+    console.log("Are you kidding?");
+}
+else if(age == 1){
+    console.log("Are you again kidding?");
 }
 
 else{
     console.log("No you cannot drive");
 }
+
+
+a = 6;
+b = 10;
+let c = a > b? (a - b) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else{
+    let c = a - b;
+}
+*/
