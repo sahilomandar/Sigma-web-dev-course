2640481c2bbab1a1486e6789327950961ab66860
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-12 22:19:44 +0530

    Learning Loop

---
+++ Video 53/index.js
@@ -0,0 +1,40 @@
+console.log("I am Learning Loop");
+
+let a = 1;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Sahil",
+//     role: "Software Developer",
+//     company: "NextGen AI"
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element)
+//     }
+// }
+
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+// for (const c of "Sahil") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
