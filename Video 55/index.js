d1a7e68a6965a254ea6ebd96fb3149b5579021e8
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-15 16:12:01 +0530

    Create a Faulty calculator using JavaScript

---
+++ Video 55/index.js
@@ -0,0 +1,14 @@
+/* Create a Faulty calculator using JavasScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+It perfoms wrong operation 10% of the times
+
+*/ 
