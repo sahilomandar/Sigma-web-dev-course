2b2dff39dd72b8f28400fc9fd816b799689f56d3
Sahil Omandar <139885049+sahilomandar@users.noreply.github.com>
2023-12-17 19:59:11 +0530

    JavaScript Strings

---
+++ Video 56/index.js
@@ -0,0 +1,27 @@
+console.log("This is Strings tutorial")
+
+let a = "Sahil";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length)
+
+let realname = "Sahil"
+let friend = "God"
+console.log("His name is " + realname + " and his friends name is " + friend)
+console.log(`His name is ${realname} and his friends name is ${friend}`)
+
+let b = "alexa";
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,3))
+console.log(b.slice(0,5))
+
+console.log(b.replace("a", "5"))
+console.log(b.concat(a, "Robot", "raj", "manoj"))
+
+console.log(b)
